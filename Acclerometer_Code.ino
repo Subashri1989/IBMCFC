3583ca6387695e566d0ae69cabdc7dd8af431970
Subashri1989 <69042609+Subashri1989@users.noreply.github.com>
2020-07-31 18:19:25 +0530

    Add files via upload

---
+++ Acclerometer_Code.ino
@@ -0,0 +1,15 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(3,4);
+
+void setup()
+{
+  mySerial.begin(9600);
+  pinMode(A1, OUTPUT);
+}
+
+void loop()
+{
+  mySerial.println(map(analogRead(A1), 332, 265, 0, 90));
+  delay(700);
+} // end loop
+
